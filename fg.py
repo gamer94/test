ccdecdb69960c45cfd96dd596e76e983f48d9d05
paramesh waran <hsemarap@gmail.com>
2014-01-01 12:23:00 +0530

    green

---
+++ fg.py
@@ -0,0 +1,35 @@
+import os
+
+#"date {month}{day}{hour}{minute}{year}"
+
+cmd = "date  %s%s1223%s"
+
+ech = "echo %c > a"
+add = "git add a"
+com = "git commit -m green"
+
+for k in [14]:
+    for j in range(4,5):
+        for  i in range(1, 20):
+            d = ""
+            m = ""
+            y = ""
+
+            if i  <  10:
+                d = "0"
+            if j  <  10:
+                m = "0"
+
+            l = i
+
+            d += str(i)
+            m += str(j)
+            y += str(k)
+
+
+
+            print cmd %(m,d,y)
+            os.system(cmd %(m, d, y))
+            os.system(ech % l)
+            os.system(add)
+            os.system(com)
